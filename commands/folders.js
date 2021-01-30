e4399aa40009628aff2185cc828620387b0ff7ef
LeonardoMussatto <23527613+LeonardoMussatto@users.noreply.github.com>
2021-01-30 11:53:50 +0100

    Implement FindByName to folders and list commands
    same fundmental logic from tasks command

--- commands/folders.js
+++ commands/folders.js
@@ -1,20 +1,96 @@
-const { getFolders } = require("../utils/ClickUpAPI_Get")
+/** GET FOLDERS
+ * command to get folders in the selected spaces, or all spaces
+ * 1. if only one arg is passed:
+ *      a. get teams' ids (aka workspaces)
+ *      b. for each team, get spaces' ids
+ *      c. get folders for each space
+ *      d. send an embed with folder name, id,lists andactive tasks
+ * 2. if more args are provided:
+ *      a. create new args to accept multiple words' space names
+ *      b. get teams' ids (aka workspaces)
+ *      c. for each team, get the spaces' ids
+ *      d. filter the spaces whose name has been passed in the args. If any match is found, add the folder to the array of targets
+ *      e. when all workspaces have been scanned for matches, if none is found return
+ *      f. get folders for each space in the target array
+ *      g. send an embed with folder name, id,lists andactive tasks
+ * //IMPORTANT spaces have to be called by name
+*/
 
-// TODO update getFolder
-// ENHANCE get folders by name
-// ENHANCE add more infos about the folder - e.g. if a chat is present
+const Discord = require("discord.js")
+const { getFolders, getTeams, getSpaces } = require("../utils/ClickUpAPI_Get")
 
 exports.run = async (client, message, args) => {
-  const folders = await getFolders(client, "6831505")
-  if (folders.err)
-    return message.channel.send(`Error fetching folders: ${folders.err}`)
-  const embed = new Discord.MessageEmbed()
-    .setTitle(`Folders in the space *~spaceNameHere~* :`)
-    .setDescription(folders.map((x) => `• ${x.name} | ${x.id}`).join("\n"))
-    .setColor("#8951FC")
-  return message.channel.send(embed)
+  if (args.length === 0) {
+    const teams = await getTeams(client)
+    for await (const team of teams) {
+      const spaces = await getSpaces(client, team.id)
+      for await (const space of spaces) {
+        const folders = await getFolders(client, space.id)
+        const embed = new Discord.MessageEmbed()
+          .setTitle(`Folders in space *${space.name}* :`)
+          .setColor("#8951fc")
+        if (folders.length < 1) {
+          embed.setDescription("It seems like there isn't any yet!")
+        } else {
+          embed.setDescription(
+            folders
+              .map(
+                (folder) =>
+                  `• **${folder.name}** - ${folder.id}  
+                   - lists: ${folder.lists.length}  
+                   - tasks: ${folder.task_count}\n`
+              )
+              .join("\n")
+          )
+        }
+        message.channel.send(embed)
+      }
+    }
+  } else {
+    const newArgs = message.content
+      .slice(client.settings.bot.prefix.length + this.help.name.length)
+      .trim()
+      .split("|")
+      .map((x) => x.trim())
+
+    const teams = await getTeams(client)
+    let targets = []
+    for await (const team of teams) {
+      const spaces = await getSpaces(client, team.id)
+      let trg = spaces.filter((space) =>
+        newArgs.some((target) => target === space.name)
+      )
+      if (trg.length > 0) {
+        targets.push(...trg)
+      }
+    }
+    if (targets.length < 1) return message.channel.send(`No match found`)
+    for await (const space of targets) {
+      const folders = await getFolders(client, space.id)
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Folders in space *${space.name}* :`)
+        .setColor("#8951fc")
+      if (folders.length < 1) {
+        embed.setDescription("It seems like there isn't any yet!")
+      } else {
+        embed.setDescription(
+          folders
+            .map(
+              (folder) =>
+                `• **${folder.name}** - ${folder.id}  
+                 - lists: ${folder.lists.length}  
+                 - tasks: ${folder.task_count}\n`
+            )
+            .join("\n")
+        )
+      }
+      message.channel.send(embed)
+    }
+  }
 }
 
 exports.help = {
   name: "folders",
+  args: 0,
+  usage: "(space) | (space) | ...",
 }
--- commands/lists.js
+++ commands/lists.js
@@ -1,41 +1,183 @@
-const { getFolder, getLists, getFolders } = require("../utils/ClickUpAPI_Get")
+/** GET LISTS
+ * command to get lists in the selected folders, or folders from all spaces
+ * 1. if only one arg is passed:
+ *      get teams' ids (aka workspaces)
+ *      a. for each team, get spaces' ids
+ *      b. get folders for each space
+ *      c. for each folder, send an embed with each list's name, id + priority level and assignees, if present
+ * 2. if more args are provided:
+ *      a. create new args to accept multiple words' list names
+ *      b. get teams' ids (aka workspaces)
+ *      c. for each team, get spaces' ids
+ *      d. for each space, get folders
+ *      d. filter the folders whose name has been passed in the args. If any match is found, add the folder to the array of targets
+ *      e. when all workspaces have been scanned for matches, if none is found return
+ *      f. get folders for each space in the target array
+ *      g. for each folder, send an embed with each list's name, id + priority level and assignees, if present
+ * //IMPORTANT folders and lists have to be called by name
+*/
 
-// TODO update getFolder
-// ENHANCE get folders and lists by name
-// ENHANCE add more info about the lists
+
+const Discord = require("discord.js")
+const { getFolders, getTeams, getSpaces } = require("../utils/ClickUpAPI_Get")
 
 exports.run = async (client, message, args) => {
-  if (args.length === 1) {
-    const folder = await getFolder(client, args[0])
-    if (folder.err)
-      return message.channel.send(`Error fetching folder: ${folder.err}`)
+  let months_arr = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ]
 
-    const lists = await getLists(client, folder.id)
-    if (lists.err)
-      return message.channel.send(`Error fetching lists: ${lists.err}`)
-
-    const embed = new Discord.MessageEmbed()
-      .setTitle(`Lists in the folder *${folder.name}* :`)
-      .setDescription(lists.map((x) => `• ${x.name} | ${x.id}`).join("\n"))
-      .setColor("#8951FC")
-    return message.channel.send(embed)
-  } else {
-    const folders = await getFolders(client, "6831505")
-    if (folders.err)
-      return message.channel.send(`Error fetching folders: ${folder.err}`)
-
-    let msg = ""
-    for (const folder of folders) {
-      const lists = folder.lists
-      msg += `\n--------\nFolder: ${folder.name} | ${folder.id}\n${lists
-        .map((l) => `  • ${l.name} | ${l.id}`)
-        .join("\n")}`
+  if (args.length === 0) {
+    const teams = await getTeams(client)
+    for await (const team of teams) {
+      const spaces = await getSpaces(client, team.id)
+      for await (const space of spaces) {
+        const folders = await getFolders(client, space.id)
+        for await (const folder of folders) {
+          const embed = new Discord.MessageEmbed()
+            .setTitle(`Lists in folder *${folder.name}* :`)
+            .setColor("#8951fc")
+          if (folder.lists.length < 1) {
+            embed.setDescription("It seems like there isn't any yet!")
+          } else {
+            embed.setDescription(
+              folder.lists
+                .map((list) => {
+                  if (!(list.assignees || list.due_date || list.priority)) {
+                    return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks`
+                  } else if (
+                    list.assignees.length &&
+                    task.due_date &&
+                    list.priority
+                  ) {
+                    let assignees = list.assignees.map((x) => x.username)
+                    let date = new Date(list.due_date * 1000)
+                    let day = date.getDate()
+                    let month = months_arr[date.getMonth()]
+                    return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks\n -  priority: ${list.priority.priority}  -  due date: ${day} ${month}\n  -  assignees: ${assignees}`
+                  } else if (
+                    list.assignees &&
+                    !list.due_date &&
+                    !list.priority
+                  ) {
+                    let assignees = list.assignees.map((x) => x.username)
+                    return `• **${list.name}** - ${list.id}  -  ${list.task_count} tasks\n -  assignees: ${assignees}`
+                  } else if (
+                    list.due_date &&
+                    !list.assignees &&
+                    !list.priority
+                  ) {
+                    let date = new Date(list.due_date * 1000)
+                    let day = date.getDate()
+                    let month = months_arr[date.getMonth()]
+                    return `• **${list.name}** - ${list.id}  -  ${list.task_count} tasks\n -  due date: ${day} ${month}`
+                  } else if (
+                    list.priority &&
+                    !list.due_date &&
+                    !list.assignees
+                  ) {
+                    return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks\n -  priority: ${list.priority.priority}`
+                  }
+                })
+                .join("\n")
+            )
+          }
+          message.channel.send(embed)
+        }
+      }
     }
-
-    return message.channel.send(`All lists:\n\`\`\`\n${msg}\n\`\`\``)
+  } else {
+    const newArgs = message.content
+      .slice(client.settings.bot.prefix.length + this.help.name.length)
+      .trim()
+      .split("|")
+      .map((x) => x.trim())
+      
+    const teams = await getTeams(client)
+    let targets = []
+    for await (const team of teams) {
+      const spaces = await getSpaces(client, team.id)
+      for await (const space of spaces) {
+        const folders = await getFolders(client, space.id)
+        let trg = folders.filter((folder) =>
+          newArgs.some((target) => target === folder.name)
+        )
+        if (trg.length > 0) {
+          targets.push(...trg)
+        }
+      }
+    }
+    if (targets.length < 1) return message.channel.send(`No match found`)
+    for await (const folder of targets) {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Lists in folder *${folder.name}* :`)
+        .setColor("#8951fc")
+      if (folder.lists.length < 1) {
+        embed.setDescription("It seems like there isn't any yet!")
+      } else {
+        embed.setDescription(
+          folder.lists
+            .map((list) => {
+              if (!(list.assignees || list.due_date || list.priority)) {
+                return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks`
+              } else if 
+              (
+                list.assignees && 
+                task.due_date && 
+                list.priority
+              ) {
+                let assignees = list.assignees.map((x) => x.username)
+                let date = new Date(list.due_date * 1000)
+                let day = date.getDate()
+                let month = months_arr[date.getMonth()]
+                return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks\n  -  priority: ${list.priority.priority}  -  due date: ${day} ${month}\n  -  assignees: ${assignees}`
+              } else if 
+              (
+                list.assignees && 
+                !list.due_date && 
+                !list.priority
+              ) {
+                let assignees = list.assignees.map((x) => x.username)
+                return `• **${list.name}** - ${list.id}  -  ${list.task_count} tasks\n  -  assignees: ${assignees}`
+              } else if 
+              (
+                list.due_date && 
+                !list.assignees && 
+                !list.priority
+              ) {
+                let date = new Date(list.due_date * 1000)
+                let day = date.getDate()
+                let month = months_arr[date.getMonth()]
+                return `• **${list.name}** - ${list.id}  -  ${list.task_count} tasks\n  -  due date: ${day} ${month}`
+              } else if 
+              (
+                list.priority && 
+                !list.due_date && 
+                !list.assignees
+              ) {
+                return `• **${list.name}** - ${list.id} -  ${list.task_count} tasks\n  -  priority: ${list.priority.priority}`
+              }
+            })
+            .join("\n")
+        )
+      }
+      message.channel.send(embed)
+    }
   }
 }
 
 exports.help = {
   name: "lists",
+  args: 0,
+  usage: "(folder) | (folder) | ...",
 }
